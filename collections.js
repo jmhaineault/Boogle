a58ffb2d269b9f2f131e86a9bd580d53de413d5f
Patricio Vargas <pevargas@valdosta.edu>
2016-11-05 04:56:01 -0400

    marker working on google apis

---
+++ collections.js
@@ -0,0 +1,30 @@
+[
+{
+    "lat": "-25.363882",
+    "lng": "131.044922",
+    "category": "cat1",
+    "title": "Heading #1",
+    "description": "desc1",
+    "imageurl": "http://ursispaltenstein.ch/blog/images/uploads_img/national_geographic_wallpaper_1.jpg",
+    "pageLink": "http://www.google.com"
+},
+{
+    "lat": "-26.363882",
+    "lng": "200.044922",
+    "category": "cat2",
+    "title": "Heading #2",
+    "description": "desc2",
+    "imageurl": "http://www.creativepics.org/wp-content/uploads/2009/10/National-Geographic-Wallpapers-002.jpg",
+    "pageLink": "http://www.google.com"
+}
+,
+{
+    "lat": "-28.363882",
+    "lng": "81.044922",
+    "category": "cat3",
+    "title": "Heading #3",
+    "description": "desc3",
+    "imageurl": "http://blog.rapidsea.com/wp-content/uploads/2008/03/20080316-bora-bora-national-geographic.jpg",
+    "pageLink": "http://www.google.com"
+}
+]
