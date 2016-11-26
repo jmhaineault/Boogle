146bb77a9f1f2f5a901c5d45aa616873bb6f2c23
jmhaineault <jmhaineault@valdosta.edu>
2016-11-25 20:02:54 -0500

    Add files via upload

--- streetview.php
+++ streetview.php
@@ -1,57 +1,64 @@
-<?php
-    /*  Connect to the mongodb database, perform query, return json results
-        Query parameters are passed via the URL as GET parameters, e.g.:
-    
-        reviews.php?id=vcNAWiLM4dR7D2nwwJ7nCA
-    */    
 
-    // Get search parameters to pass to the query function
-    $lat = $_GET['latitude'];
-    $lng = $_GET['longitude'];
-
-    // Set the mongodb host and database names
-    $dbhost = 'localhost';
-    $dbname = 'boogle';
-
-    // Connect to the mongodb database
-    $m = new MongoClient();
-    $db = $m->$dbname;
-
-    // Set content type in header for easy viewing in browser
-    header("Content-type: application/json");
-
-    if(isset($lat,$lng)){
-        search($db, $lat, $lng);
-    }
-    else{
-        echo "Missing required parameter(s)";
-    }
-
-    // Performs the mongodb query and returns results in json encoded format
-    function search($db, $lat, $lng){
-        $query = array('latitude' => $lat,
-                        'longitude' => $lng);
-                        
-        $result = $db->review->find($query);
-        echo json_encode(iterator_to_array($result, false));
-    }
-
-?>
-<script type="text/javascript">
-function initialize() {
-  var latlng = {lat: $lat, lng: $lng};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    center: latlng,
-    zoom: 14
-  });
-  var panorama = new google.maps.StreetViewPanorama(
-      document.getElementById('pano'), {
-        position: latlng,
-        pov: {
-          heading: 34,
-          pitch: 10
-        }
-      });
-  map.setStreetView(panorama);
-}
-</script>
+<!DOCTYPE html>
+<html class="noIE" lang="en-US">
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale = 1.0, maximum-scale=1.0, user-scalable=no"/>
+        <title>Boogle</title>
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/ionicons.min.css">
+        <link rel="stylesheet" href="assets/css/owl.carousel.css">
+        <link rel="stylesheet" href="assets/css/owl.theme.css">
+        <link rel="stylesheet" href="assets/css/flexslider.css" type="text/css">
+        <link rel="stylesheet" href="assets/css/main.css">
+        <link rel="stylesheet" href="assets/css/login.css">
+        <link href="http://emojicss.com/emoji.css" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">  
+         <style>
+          html, body {
+            height: 100%;
+            margin: 0;
+            padding: 0;
+          }
+          #map, #pano {
+            float: left;
+            height: 100%;
+            width: 45%;
+          }
+        </style>
+        </head>
+        <body>
+        <div id="map"></div>
+        <div id="pano"></div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBRyIx95DIw0i8hWbd9i5R61UVBJTeQhtw&callback=initMap"
+        async defer></script>
+        <script type="text/javascript" src="gmaps.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBRyIx95DIw0i8hWbd9i5R61UVBJTeQhtw&callback=initMap"
+        async defer></script>
+         <script type="text/javascript" src="gmaps.js"></script>
+        <script type="text/javascript">
+        (function initialize() {
+          var map;
+          var latlng = {lat: "<?php echo $lat ?>", lng: "<?php echo $lng ?>"};
+          map = new google.maps.Map(document.getElementById('map'), {
+            center: latlng,
+            zoom: 14
+          });
+          var panorama = new google.maps.StreetViewPanorama(
+              document.getElementById('pano'), {
+                position: latlng,
+                pov: {
+                  heading: 34,
+                  pitch: 10
+                }
+              });
+          map.setStreetView(panorama);
+        };
+        </script>
+        <script async defer
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBRyIx95DIw0i8hWbd9i5R61UVBJTeQhtw&callback=initialize">
+        </script>
+        </body>
+</html>
